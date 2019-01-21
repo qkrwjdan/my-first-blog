9ec1084a96b059b4db259e320810dc0e15bc21e9
jungmu Park <madogisa12@naver.com>
2019-01-22 00:49:09 +0900

    My DjangoGirls app. first commit

---
+++ blog/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import redirect, get_object_or_404,render
+from .models import Post
+from django.utils import timezone
+from .forms import PostForm
+# Create your views here.
+
+def post_list(request):
+    qs = Post.objects.all()
+    qs = qs.filter(published_date__lte=timezone.now())
+    qs = qs.order_by('published_date')
+    
+    return render(request,'blog/post_list.html',{
+        'post_list':qs,
+    })
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'blog/post_detail.html',{
+        'post' : post,
+    })
+
+# @Login_required
+def post_new(request):
+    #request.POST , request.FILES
+
+    if request.method == 'POST':
+        form = PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail',post.pk)
+    else:
+        form = PostForm()
+
+    return render(request,'blog/post_edit.html',{
+        'form' : form,
+    })
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post,pk=pk)
+
+    if request.method == 'POST':
+            form = PostForm(request.POST,request.FILES, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail',post.pk)
+    else:
+        form = PostForm(instance = post)
+
+    return render(request,'blog/post_edit.html',{
+        'form' : form,
+    })
